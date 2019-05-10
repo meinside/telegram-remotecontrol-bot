58aee051a6525c8085a39cb01ed846d3b9bc66ee
Sungjin Han <meinside@gmail.com>
2019-05-10 14:36:24 +0900

    Fix cmd/telegram-bot-broadcast to use default port number

--- cmd/telegram-bot-broadcast/main.go
+++ cmd/telegram-bot-broadcast/main.go
@@ -22,21 +22,25 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) > 0 {
-		// read variables from config file
+		var cliPort int
+
+		// read port number from config file
 		if config, err := helper.GetConfig(); err == nil {
-			cliPort := config.CliPort
+			cliPort = config.CliPort
 			if cliPort <= 0 {
 				cliPort = conf.DefaultCliPortNumber
 			}
-
-			message := strings.Join(args, " ")
-
-			if _, err := http.PostForm(fmt.Sprintf("http://localhost:%d%s", cliPort, conf.HttpBroadcastPath), url.Values{
-				conf.ParamMessage: {message},
-			}); err != nil {
-				fmt.Println(fmt.Errorf("*** %s", err))
-			}
 		} else {
+			fmt.Printf("failed to load config, using default port number: %d (%s)", conf.DefaultCliPortNumber, err)
+
+			cliPort = conf.DefaultCliPortNumber
+		}
+
+		message := strings.Join(args, " ")
+
+		if _, err := http.PostForm(fmt.Sprintf("http://localhost:%d%s", cliPort, conf.HttpBroadcastPath), url.Values{
+			conf.ParamMessage: {message},
+		}); err != nil {
 			fmt.Println(fmt.Errorf("*** %s", err))
 		}
 	} else {
